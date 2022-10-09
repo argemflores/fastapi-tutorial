488edc45aa71cf0e14b7f41d34bcd6c3e3834fa7
Argem Flores <argemflores@gmail.com>
2022-10-09 14:55:40 +0800

    Try predefined enum values

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 """Imports"""
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -48,3 +54,22 @@ async def read_user(user_id: int):
         json: User ID
     """
     return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    """Get model
+
+    Args:
+        model_name (ModelName): Model name
+
+    Returns:
+        json: Model
+    """
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
