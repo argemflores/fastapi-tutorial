3342d573a06ae974eb22f26cc5ee329575132963
Argem Flores <argemflores@gmail.com>
2022-10-09 14:44:29 +0800

    Add path parameters

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ async def root():
         json: Response
     """
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    """Read item
+
+    Args:
+        item_id (str): Item ID
+
+    Returns:
+        json: Response
+    """
+    return {"item_id": item_id}
